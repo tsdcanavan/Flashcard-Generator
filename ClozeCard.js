24c9d0405bb1d720c62697ee3d358d288191ef5d
unknown <tsdcanavan@yahoo.com>
2017-10-10 10:39:38 -0400

    working to understand prototype

---
+++ ClozeCard.js
@@ -0,0 +1,14 @@
+var ClozeCard = function (text, cloze) {
+    this.text = text.split(cloze);
+    this.cloze = cloze;
+};
+
+function ClozeCardPrototype() {
+    this.clozeRemoved = function () {
+        return `${this.text[0]} ... ${this.text[1]}`;
+    };
+};
+
+ClozeCard.prototype = new ClozeCardPrototype();
+
+module.exports = ClozeCard;
